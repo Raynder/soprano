2bb6d1e904f4d7570288d4539a65b5583f5d89b5
Raynder <raynder4@gmail.com>
2022-01-18 00:24:01 -0300

    animação

--- index.php
+++ index.php
@@ -31,6 +31,9 @@
     </header>
 
     <section class="home">
+        <video class="video-slide active" src="1.mp4" autoplay muted loop></video>
+        <video class="video-slide" src="1.mp4" autoplay muted loop></video>
+
         <div class="content">
             <h1>Wonderful.<br><span>Island</span></h1>
             <p>Agora você pode navegar com privacidade, e as outras pessoas que usarem este dispositivo não verão suas atividades. No entanto, os downloads, favoritos e itens da Lista de leitura ainda serão salvos. Saiba mais</p>
@@ -47,7 +50,55 @@
                 <i class="fab fa-twitter"></i>
             </a>
         </div>
+
+        <div class="slider-navigation">
+            <div class="nav-btn active"></div>
+            <div class="nav-btn"></div>
+            <div class="nav-btn"></div>
+            <div class="nav-btn"></div>
+        </div>
     </section>
+
+    <script type="text/javascript">
+        const menuBtn = document.querySelector('.menu-btn');
+        const navigation = document.querySelector('.navigation');
+
+        menuBtn.addEventListener('click', () => {
+            menuBtn.classList.toggle('active');
+            navigation.classList.toggle('active');
+        });
+
+        //Javascript for video slider navigation
+        const btns = document.querySelectorAll('.nav-btn');
+        const slides = document.querySelectorAll('.video-slide');
+
+        var sliderNav = function(manual){
+            btns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            slides.forEach((slide) => {
+                slide.classList.remove('active');
+                slide.classList.remove('trans');
+            });
+
+            btns[manual].classList.add('active');
+            slides[manual].classList.add('active');
+            setTimeout(() => {
+                slides[manual].classList.add('trans');
+                slides[manual].classList.add('active2');
+            }, 300);
+            setTimeout(() => {
+                slides[manual].classList.remove('active2');
+                slides[manual].classList.add('active');
+            }, 1200);
+        }
+
+        btns.forEach((btn, index) => {
+            btn.addEventListener('click', () => {
+                sliderNav(index);
+            });
+        });
+    </script>
 
 </body>
 
