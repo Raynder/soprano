a90b1995c1eaa5105406849f26b04ed777471155
Raynder <raynder4@gmail.com>
2022-01-18 13:11:53 +0000

    template inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soprano Coral & Orquestra</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <a href="" class="brand">teste</a>
+
+        <div class="navigation">
+            <div class="navigation-itens">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Explore</a>
+                <a href="">Contato</a>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
